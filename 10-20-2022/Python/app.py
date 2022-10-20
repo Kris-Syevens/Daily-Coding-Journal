6fe2d4ffcf0b50d54ad14c82fec4099e969fdcf6
Kris <kris@syevens.com>
2022-10-20 18:10:22 -0500

    Update Commit
    Added challenge 1 and 2 for Python.

--- 10-20-2022/Python/app.py
+++ 10-20-2022/Python/app.py
@@ -1,37 +1,50 @@
 #Codewar Kata
 
-# Challenge # 1 -
+# Challenge # 1 - Write a function which calculates the average of the numbers in a given list.
 
-# ENTER CODE HERE
+def find_average(numbers):
+    return sum(numbers) / len(numbers)
+    
+
 
 #Test log
-# print();
-# print();
-# print();
+print(find_average([1, 2, 3]))
+print(find_average([2, 2, 3, 3]))
+print(find_average([5, 2, 3, 10]))
 
 # Better method of completing challenge:
+
+# def find_average(array):
+#     return sum(array) / len(array) if array else 0
 
 ###############################################################################
 
 
 #Codewar Kata
 
-# Challenge # 1 -
+# Challenge # 2 - Simple, remove the spaces from the string, then return the resultant string.
 
-# ENTER CODE HERE
+def no_space(x):
+    return x.replace(" ", "")
+
+
+
+
 
 #Test log
-# print();
-# print();
-# print();
+print(no_space('8 j 8   mBliB8g  imjB8B8  jl  B'))
+print(no_space('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'))
+print(no_space('8aaaaa dddd r     '))
 
 # Better method of completing challenge:
+
+# *** I RETURNED THE IDEAL CODE :) ***
 
 ###############################################################################
 
 #Codewar Kata
 
-# Challenge # 1 -
+# Challenge # 3 -
 
 # ENTER CODE HERE
 
