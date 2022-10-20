0bed5594fbc59f6094d2c30df749e9f8579d9e9a
Kris <kris@syevens.com>
2022-10-20 17:05:03 -0500

    Update Commit
    Added JS challenge 1.

--- 10-20-2022/JavaScript/script.js
+++ 10-20-2022/JavaScript/script.js
@@ -1,15 +1,50 @@
 //Codewar Kata
 
-// Challenge # 1 -
+// Challenge # 1 - Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
-// ENTER CODE HERE
+function countSheeps(arrayOfSheep) {
+  let sum = 0;
+  for (let sheep of arrayOfSheep) {
+    if (sheep === true) sum += 1;
+  }
+  return sum;
+}
 
 //Test log
-// console.log();
-// console.log();
-// console.log();
+console.log(
+  countSheeps([
+    true,
+    true,
+    true,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    true,
+    true,
+  ])
+);
 
 // Better method of completing challenge:
+
+// function countSheeps(arrayOfSheeps) {
+//   return arrayOfSheeps.filter(Boolean).length;
+// }
 
 ////////////////////////////////////////////////////////////////////////
 
