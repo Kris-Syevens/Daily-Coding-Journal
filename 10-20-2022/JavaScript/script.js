f3f9da9726199a44a029e2fc23cb3a09fc81fee1
Kris <kris@syevens.com>
2022-10-20 17:37:30 -0500

    Update Commit
    Added challenge 2 and 3 for JS.

--- 10-20-2022/JavaScript/script.js
+++ 10-20-2022/JavaScript/script.js
@@ -50,30 +50,52 @@ console.log(
 
 //Codewar Kata
 
-// Challenge # 2 -
+// Challenge # 2 - Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
 
-// ENTER CODE HERE
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 === 0 && flower2 % 2 !== 0) {
+    return true;
+  } else if (flower1 % 2 !== 0 && flower2 % 2 === 0) {
+    return true;
+  } else return false;
+}
 
 //Test log
-// console.log();
-// console.log();
-// console.log();
+console.log(lovefunc(1, 4));
+console.log(lovefunc(2, 2));
+console.log(lovefunc(0, 1));
+console.log(lovefunc(0, 0));
 
 // Better method of completing challenge:
+
+// function lovefunc(flower1, flower2) {
+//   return flower1 % 2 !== flower2 % 2;
+// }
 
 ////////////////////////////////////////////////////////////////////////
 
 //Codewar Kata
 
-// Challenge # 3 -
+// Challenge # 3 - Convert number to reversed array of digits Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 
-// ENTER CODE HERE
+function digitize(n) {
+  return n
+    .toString()
+    .split("")
+    .map((n) => (n = Number(n)))
+    .reverse();
+}
 
 //Test log
-// console.log();
-// console.log();
-// console.log();
+console.log(digitize(35231));
+console.log(digitize(0));
 
 // Better method of completing challenge:
+
+// function digitize(n) {
+//   return String(n).split("").map(Number).reverse();
+// }
 
 ////////////////////////////////////////////////////////////////////////
