dc662004d147db156403e9764b235cd8e3179933
Kris <kris@syevens.com>
2022-10-25 23:37:47 -0500

    Update Commit
    Added challenges in TS.

--- 10-25-2022/TypeScript/script.ts
+++ 10-25-2022/TypeScript/script.ts
@@ -1,13 +1,40 @@
 //Codewar Kata
 
-// Challenge # 1 -
+// Challenge # 1 - Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
 
-// ENTER CODE HERE
+// The function takes a name as its only argument, and returns one of the following strings:
+
+function areYouPlayingBanjo(name: string): string {
+  return name.toLowerCase()[0] === "r"
+    ? `${name} plays banjo`
+    : `${name} does not play banjo`;
+}
 
 //Test log
-// console.log();
-// console.log();
-// console.log();
+console.log(areYouPlayingBanjo("Adam"));
+console.log(areYouPlayingBanjo("Paul"));
+console.log(areYouPlayingBanjo("Ringo"));
+console.log(areYouPlayingBanjo("raul"));
+
+// Better method of completing challenge:
+
+// Executed best possible practice at the time
+
+////////////////////////////////////////////////////////////////////////
+
+//Codewar Kata
+
+// Challenge # 2 - Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name: string): string {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+//Test log
+console.log(greet("Ryan"));
+console.log(greet("Shingles"));
+console.log();
 
 // Better method of completing challenge:
 
@@ -15,24 +42,11 @@
 
 //Codewar Kata
 
-// Challenge # 2 -
+// Challenge # 3 - Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
 
-// ENTER CODE HERE
-
-//Test log
-// console.log();
-// console.log();
-// console.log();
-
-// Better method of completing challenge:
-
-////////////////////////////////////////////////////////////////////////
-
-//Codewar Kata
-
-// Challenge # 3 -
-
-// ENTER CODE HERE
+function doubleChar(str: string): string {
+  // Your code here
+}
 
 //Test log
 // console.log();
