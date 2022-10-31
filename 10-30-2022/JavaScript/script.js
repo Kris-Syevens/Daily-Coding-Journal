3103bdecc943c510d94d4583463f0b7987eac343
Kris <kris@syevens.com>
2022-10-30 23:48:44 -0500

    Update Commit
    Added 10-30 challenge.

---
+++ 10-30-2022/JavaScript/script.js
@@ -0,0 +1,34 @@
+//Codewar Kata
+
+// Challenge # 1 - You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples
+
+// function likes(names) {
+//   // TODO
+// }
+
+//Test log
+// console.log(likes([]));
+// console.log(llikes(["Peter"]));
+// console.log(likes([]));
+// console.log(likes([]));
+
+// Better method of completing challenge:
+
+////////////////////////////////////////////////////////////////////////
+
+//Codewar Kata
+
+// Challenge # 2 - Hello world!
+
+function hello(name) {
+  return `hello ${name}!`;
+}
+
+//Test log
+console.log(hello("kris"));
+
+// Better method of completing challenge:
+
+////////////////////////////////////////////////////////////////////////
